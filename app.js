92500c028f59b8fa03c8d1bf7b9c01e304025f27
Thomas Rogers <Mr.Thomas.Rogers@Proton.me>
2024-12-23 14:38:08 +0000

    some small refactors to handle errors

--- app.js
+++ app.js
@@ -3,12 +3,8 @@ const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 const mongo_uri = "mongodb://localhost:27017/moviesdb";
 
-mongoose.connect(mongo_uri)
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log("MongoDB not Connected: " + err));
-
 const movieSchema = new mongoose.Schema({
-    title: { type: String, required: true },
+    title: { type: String, required: true, unique: true },
     releaseYear: { type: Number, required: true },
     rated: { type: String, required: false },
     actors: { type: String, requred: true },
@@ -29,92 +25,124 @@ const Movie = mongoose.model('Movie', movieSchema);
 
 const movieNames = [
   "The Shawshank Redemption",
-//   "The Godfather",
-//   "Pulp Fiction",
-//   "The Dark Knight",
-//   "Fight Club",
-//   "Inception",
-//   "Goodfellas",
-//   "The Matrix",
-//   "Forrest Gump",
-//   "The Silence of the Lambs",
-//   "Saving Private Ryan",
-//   "Jurassic Park",
-//   "Titanic",
-//   "The Lion King",
-//   "Back to the Future",
-//   "Die Hard",
-//   "Raiders of the Lost Ark",
-//   "The Terminator",
-//   "Alien",
-//   "Jaws",
-//   "Schindler's List",
-//   "The Lord of the Rings: The Fellowship of the Ring",
-//   "The Lord of the Rings: The Two Towers",
-//   "The Lord of the Rings: The Return of the King",
-//   "The Green Mile",
-//   "Gladiator",
-//   "The Departed",
-//   "The Usual Suspects",
-//   "Interstellar",
-//   "The Sixth Sense",
-//   "Se7en",
-//   "The Prestige",
-//   "Avatar",
-//   "Memento",
-//   "Star Wars: Episode IV - A New Hope",
-//   "Star Wars: Episode V - The Empire Strikes Back",
-//   "Star Wars: Episode VI - Return of the Jedi",
-//   "The Avengers",
-//   "Iron Man",
-//   "Black Panther",
-//   "The Social Network",
-//   "No Country for Old Men",
-//   "The Wolf of Wall Street",
-//   "Inglourious Basterds",
-//   "Django Unchained",
-//   "The Big Lebowski",
-//   "Good Will Hunting",
-//   "A Beautiful Mind",
-//   "The Pianist",
-//   "Braveheart"
+  "Star Wars: Episode IV - A New Hope",
+  "Star Wars: Episode V - The Empire Strikes Back",
+  "Star Wars: Episode VI - Return of the Jedi",
+  "The Godfather",
+  "Pulp Fiction",
+  "The Dark Knight",
+  "Fight Club",
+  "Inception",
+  "Goodfellas",
+  "The Matrix",
+  "Forrest Gump",
+  "The Silence of the Lambs",
+  "Saving Private Ryan",
+  "Jurassic Park",
+  "Titanic",
+  "The Lion King",
+  "Back to the Future",
+  "Die Hard",
+  "Raiders of the Lost Ark",
+  "The Terminator",
+  "Alien",
+  "Jaws",
+  "Schindler's List",
+  "The Lord of the Rings: The Fellowship of the Ring",
+  "The Lord of the Rings: The Two Towers",
+  "The Lord of the Rings: The Return of the King",
+  "The Green Mile",
+  "Gladiator",
+  "The Departed",
+  "The Usual Suspects",
+  "Interstellar",
+  "The Sixth Sense",
+  "Se7en",
+  "The Prestige",
+  "Avatar",
+  "Memento",
+  "The Avengers",
+  "Iron Man",
+  "Black Panther",
+  "The Social Network",
+  "No Country for Old Men",
+  "The Wolf of Wall Street",
+  "Inglourious Basterds",
+  "Django Unchained",
+  "The Big Lebowski",
+  "Good Will Hunting",
+  "A Beautiful Mind",
+  "The Pianist",
+  "Braveheart"
 ];
 
 const getMovieDetails = async (movieNames) => {
+    await mongoose.connect(mongo_uri)
+        .then(() => console.log('MongoDB Connected'))
+        .catch(err => console.log("MongoDB not Connected: " + err));
+
+    console.log("Adding Movies...");
     movieNames.forEach(async (movieName) => {
-        // console.log(movieName)
-        const movieSearchResponse = await fetch(`https://www.omdbapi.com/?s=${movieName}&apikey=c57fa46a`);
-        console.log(movieSearchResponse.ok);
-        if (!movieSearchResponse.ok) return;
+        console.log(`Adding ${movieName}`);
 
-        const movieSearch = await movieSearchResponse.json();
-        // console.log(movieSearch);
-        const firstMovie = movieSearch.Search[0];
-        // console.log(firstMovie);
-        
-        const movieDetails = await fetch(`https://www.omdbapi.com/?i=${firstMovie.imdbID}&apikey=c57fa46a`);
-        const movieData = await movieDetails.json();
-        console.log(movieData)
+        try {
+            const searchResponse = await fetch(`https://www.omdbapi.com/?s=${movieName}&apikey=c57fa46a`);
 
-        //calc rating
-        const imdbRaiting = Number(movieData.imdbRating);
-        console.log(movieData.imdbRating);
+            if (!searchResponse.ok) {
+                console.warn(`Response not ok!`)
+                return;
+            }
 
-        const actualRaiting = Math.round(imdbRaiting / 2);
-        
-        const newMovie = new Movie({
-            title: movieData.Title,
-            releaseYear: movieData.Year,
-            rated: movieData.Rated,
-            actors: movieData.Actors,
-            plot: movieData.Plot,
-            director: movieData.Director,
-            stars: actualRaiting,
-            posterUrl: movieData.Poster
-        });
+            const movieSearch = await searchResponse.json();
+            if (movieSearch.Response !== "True") {
+                console.warn(`Failed Response from server (${movieName}): ` + movieSearch.Error);
+                return;
+            }
 
-        newMovie.save();
+            const foundMovie = movieSearch?.Search[0];
+
+            if (foundMovie === null) {
+                console.warn(`Could not find ${movieName} (null)`);
+                return;
+            }
+            
+            const movieResponse = await fetch(`https://www.omdbapi.com/?i=${foundMovie.imdbID}&apikey=c57fa46a`);
+            const movieData = await movieResponse.json();
+
+            const searchMovie = await Movie.findOne({
+                title: { $regex: movieData.Title }
+            });
+
+            if (searchMovie !== null) {
+                console.warn(`Database already has that movie: ${movieData.Title}`);
+                return;
+            }
+
+            const scaledDownRaiting = Math.round(Number(movieData.imdbRating) / 2);
+            
+            const newMovie = new Movie({
+                title: movieData.Title,
+                releaseYear: movieData.Year,
+                rated: movieData.Rated,
+                actors: movieData.Actors,
+                plot: movieData.Plot,
+                director: movieData.Director,
+                stars: scaledDownRaiting,
+                posterUrl: movieData.Poster
+            });
+
+            await newMovie.save();
+
+            console.log(`Movie '${movieName}' added`);
+            return;
+        } catch (err) {
+            console.warn("Something went wrong adding movie: ", movieName);
+            console.error(err);
+            return;
+        }
+
     });
 }
 
 getMovieDetails(movieNames);
+// console.log(movieNames.length)
